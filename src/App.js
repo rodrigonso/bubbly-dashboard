d3331df2ecaa4599a3e86f0b227739cad0bcd1ba
Rodrigo Mascarenhas <Rodrigo@erics-imac.attlocal.net>
2020-03-28 20:16:10 -0500

    initial project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { Layout, Menu } from "antd";
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined
+} from "@ant-design/icons";
+
+const { Header, Sider, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={false}>
+        <div
+          className="logo Here"
+          style={{ padding: 60, color: "#fff", textAlign: "center" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Logo
+        </div>
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+          <Menu.Item key="1">
+            <UserOutlined />
+            <span>nav 1</span>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <VideoCameraOutlined />
+            <span>nav 2</span>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <UploadOutlined />
+            <span>nav 3</span>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          className="site-layout-background"
+          style={{ padding: 0, backgroundColor: "#fff" }}
+        ></Header>
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
   );
 }
 
